b03f08a4dd6c33c7514d4e22379df221dac3872f
AleAlle62 <basketallegrini@gmail.com>
2022-11-11 17:12:09 +0100

    fine + bonus

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+const app = new Vue ({
+    //qui inserisco il collegamento all'ID
+    el: '#root',
+    //qui inserisco i dati
+    data: {
+        primaClasse: 'stile', //questa è la creazione della classe assegnata al css
+        secondaClasse: 'https://static.fanpage.it/wp-content/uploads/sites/6/2021/07/coughing-cat-b.jpg' //questa è l'immagine
+    }
+    
+})
